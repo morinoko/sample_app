6b971dc915ccb663c9776c1e28794cb5c64a213e
morinoko <forby.1@gmail.com>
2013-07-07 09:06:17 -0400

    fix up spec tests

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module SampleApp
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -23,5 +24,6 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+      config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,62 +2,43 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
+  subject { page }
+  
+  shared_examples_for "all static pages" do	
+  	it { should have_content(heading)}
+  	it { should have_title(full_title(page_title))}
+  end
+  
   describe "Home page" do
+  	before { visit root_path }
+  	let(:heading) { 'Sample App' }
+  	let(:page_title) { '' }
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+	before { visit help_path }
+	let(:heading) { 'Help' }
+  	let(:page_title) { 'Help' }
+	
+  	it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+	before { visit about_path }
+	let(:heading) { 'About' }
+  	let(:page_title) { 'About Us' }
+   
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
-
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+	before { visit contact_path }
+	let(:heading) { 'Contact' }
+  	let(:page_title) { 'Contact' }
+	
+    it_should_behave_like "all static pages"
   end
 end
