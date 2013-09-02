48787e220950a01f6f0d13b13264247c1d277755
morinoko <forby.1@gmail.com>
2013-09-02 00:41:25 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name		"Felice Forby"
-		email		"forby.1@gmail.com"
-		password 	"sharky00"
-		password_confirmation "sharky00"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+    	admin true
+    end
+  end
 end
